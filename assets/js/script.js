1615ceffc62147fc4cfa45229ec9cc9b6b6c2532
sdmnny <sdmnny@gmail.com>
2022-05-12 18:16:33 -0400

    added variables for special, numerical, upper, lower

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +12,30 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+funtion generatePassword () {
+  var passwordLength = Math.floor (
+    prompt (
+      "How long do you want the password to be? Choose a password between 8 to 128 characters"
+
+    )
+  );
+if (!NaN(passwordLength) && passwordLength >= 8 && passwordLength <=128) {
+  var specialChar = confrim("Do you special character?");
+  var numericalChar = confirm("Do you want numerical characters?");
+  var upperChar = confirm("Do you want uppercase characters?");
+  var lowerChar = confirm ("DO you wnat lowercase characters?");
+  if (
+    !specialChar &&
+    !numericalChar &&
+    !upperChar &&
+    !lowerChar
+  ) {
+    alert ("Choose at least one options!");
+    var wantToContinue = confirm("Do you still want to generate a password?");
+    if (wantToContinue) {
+      generatePassword ();
+    }
+  } else {
+    var finalPassword = [""]
+  }
+
