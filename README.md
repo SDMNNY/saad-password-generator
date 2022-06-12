e275086573c7cef6531f7780bebfedf2dc983df8
sdmnny <sdmnny@gmail.com>
2022-06-12 12:11:20 -0400

    editing script.js

--- README.md
+++ README.md
@@ -1,3 +1,66 @@
 # saad-password-generator
 Welcome to my Password Generator! 
 My first attempt to create a password generator, which shuffles between lowercase, uppercase, numericals, and special characters to give you an awesome password!
+
+
+# README Generator
+
+![License](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+Would you like to automate your ReadMe!
+The purpose behind this application is to allow hard working coders to have a sense of ease. Using this generator will help erase burden for coders during long and stressful projects, this application will create a readme instantly! 
+Give it a try!!!
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Tests](#tests)
+4. [Questions](#questions)
+5. [License](#license)
+
+## Installation
+
+In order to install this application and use it, you would need to install inquirer.
+
+To do so, you would have to run the following after you clone the files down using SSH Keys.
+
+```
+Enter: npm i
+
+```
+
+This will install inquirer and you will be be able to generate a professional readme.
+
+## Usage
+
+In order to run the application, you would need to run the following in your terminal/bash.
+
+```
+Enter: node index
+
+```
+
+You will then be prompted with questions to help you generate your professional readme.
+
+### Please see the gif below
+
+![Gif of me demonstrating how to use this program on terminal](assets/readme.gif )
+
+
+## Tests
+*PENDING
+
+## Questions
+
+If you have any questions, please feel free to reach out to me via:
+
+1. Email: sdmnny@gmail.com
+2. GitHub: SDMNNY
+
+## License
+
+## Credits
+
+Copyright © SDMNNY All rights reserved.
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,69 +5,12 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePasword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
 }
 
-function generatePassword () {
-  var passwordLength = Math.floor(
-    prompt (
-      "How long do you want the password to be? Choose a password between 8 to 128 characters"
-
-    )
-  );
-if (!NaN(passwordLength) && passwordLength >= 8 && passwordLength <=128) {
-  var specialChar = confrim("Do you special character?");
-  var numericalChar = confirm("Do you want numerical characters?");
-  var upperChar = confirm("Do you want uppercase characters?");
-  var lowerChar = confirm ("DO you wnat lowercase characters?");
-  if (
-    !specialChar &&
-    !numericalChar &&
-    !upperChar &&
-    !lowerChar
-  ) {
-    alert("Choose at least one options!");
-    var wantToContinue = confirm("Do you still want to generate a password?");
-    if (wantToContinue) {
-      generatePassword ();
-    }
-  } else {
-    var finalPassword = [""];
-    var finalPasswordPool = "";
-    var specialPool = "“‘!#$%&*+,-./:;<=>?@()[]^_`{|}~“";
-    var numbersPool = "1234567890";
-    var upperCasePool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowerCasePool = "abcdefghijklmnopqrstuvwxyz";
-    if (specialChar) {
-      finalPasswordPool += specialPool;
-    }
-    if (numericalChar) {
-      finalPasswordPool += upperCasePool;
-    }
-    if (lowerChar) {
-      finalPasswordPool += lowerCasePool;
-    }
-    var finalPasswordPool = finalPasswordPool.split("");
-    for (var i = 0; i < passwordLength; i++){
-      finalPassword = finalPassword.concat(
-        finalPasswordPool [
-          Math.floor(Math.random() * finalPasswordPool.length)
-        ]
-      );
-    }
-    return finalPassword.join("");
-    }
-  } else {
-    alert("Please choose a valid number between 8 and 128.");
-    var wantToContinue = confirm("Do you still want to generate?");
-    if (wantToContinue){
-      generatePassword();
-    
-    }
-  }
-}
-
+// Arrays for passwords 
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
