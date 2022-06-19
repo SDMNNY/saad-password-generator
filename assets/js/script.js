96889aebb65e94727d78046d1b3c492ce5610cf9
sdmnny <sdmnny@gmail.com>
2022-06-19 19:36:41 -0400

    completed script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,82 @@ function writePassword() {
 
 // Arrays for passwords 
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
 var specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var upperArray = Array(lower.toUpperCase());
+var lowerArray = Array(lower);
+var numberArray = Array(numbers);
+var specialCharactersArray = Array(specialCharacters);
+
+
+// Blank Arrays 
+
+var generatePassword = function () { 
+  var password = "";
+  var finalArr = ""; 
+
+
+  // Prompt to confirm password length 
+
+  var confirmLength = parseInt(
+    prompt('PLEASE SPECIFY THE NUMBER OF CHARACTERS YOU WOULD LIKE FOR YOUR PASSWORD. PASSWORD MUST BE 8-128 characters.')
+  );
+
+  // WHILE loop to confirm user choice 
+
+  if (isNaN(confirmLength)) { 
+    alert('PLEASE TRY AGAIN');
+    return;
+  }
+
+  // confirmations to add characters 
+
+  var confirmLowerCase = confirm( 
+    "Include lower case characters for your password?"
+  );
+
+  var confirmCapitalCase = confirm( 
+    "Include capital case characters for your password?"
+  );
+
+  var confirmSpecialChar = confirm( 
+    "Include special characters for your password?"
+  );
+
+  var confirmNumber = confirm(
+    "Include numbers for your password?"
+  );
+
+  // create final arrays based of user 
+
+  if (confirmLowerCase) {
+    finalArr = finalArr.concat(lowerArray);
+  }
+
+  if (confirmCapitalCase) {
+    finalArr = finalArr.concat(upperArray);
+  }
+
+  if (confirmSpecialChar) {
+    finalArr = finalArr.concat(specialCharacters);
+  }
+
+  if (confirmNumber) {
+    finalArr = finalArr.concat(numberArray);
+  }
+
+
+  // combine final array with random element
+
+  for (i = 0; i < confirmLength; i++) {
+    password = password.concat(
+      finalArr.charAt(Math.floor(Math.random() * finalArr.length))
+    );
+  }
+
+  return password;
+};
+
+
+// Event Listener to generate button 
+generateBtn.addEventListener("click", writePassword);
